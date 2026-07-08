c910fa7c476506b0db3afd435916855476d86381
Berserqq <pau4okgg@gmail.com>
2026-07-08 21:54:25 +0300

    v1.0
    Собран билд

---
+++ build/CMakeFiles/sokoban.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/berserq/Documents/GitHub/Sokoban/src/game.c" "CMakeFiles/sokoban.dir/src/game.c.o" "gcc" "CMakeFiles/sokoban.dir/src/game.c.o.d"
+  "/home/berserq/Documents/GitHub/Sokoban/src/input.c" "CMakeFiles/sokoban.dir/src/input.c.o" "gcc" "CMakeFiles/sokoban.dir/src/input.c.o.d"
+  "/home/berserq/Documents/GitHub/Sokoban/src/level.c" "CMakeFiles/sokoban.dir/src/level.c.o" "gcc" "CMakeFiles/sokoban.dir/src/level.c.o.d"
+  "/home/berserq/Documents/GitHub/Sokoban/src/main.c" "CMakeFiles/sokoban.dir/src/main.c.o" "gcc" "CMakeFiles/sokoban.dir/src/main.c.o.d"
+  "/home/berserq/Documents/GitHub/Sokoban/src/menu.c" "CMakeFiles/sokoban.dir/src/menu.c.o" "gcc" "CMakeFiles/sokoban.dir/src/menu.c.o.d"
+  "/home/berserq/Documents/GitHub/Sokoban/src/render.c" "CMakeFiles/sokoban.dir/src/render.c.o" "gcc" "CMakeFiles/sokoban.dir/src/render.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
