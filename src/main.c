21584aa0be4f28cbf814c72b97fb786617d6d95e
Berserqq <pau4okgg@gmail.com>
2026-07-14 18:01:51 +0300

    Documentary, level ending screen improvement

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>>
 #include <string.h>
 
 #include "../include/level.h"
@@ -36,13 +37,17 @@ int main(void){
     //Основной цикл
     while (1)
     {
+        //Присвоение рекордов уровней для меню
         for(int i = 0; i < level_count; i++) {
             passed[i] = (save[i].best_moves > 0) ? 1 : 0;
+            levels[i].best_moves = save[i].best_moves;
         }
+
+        //Показ меню выбора уровня и получение ввода пользователя
         int selected = select_level(levels, passed, level_count);
-        
-        // strcpy(save[selected].level_name, levels[selected].name);
+        //Запуск выбранного уровня
         start_game(levels[selected].path, &save[selected]);
+        //Сохранения рекордов после прохождения уровня или выхода из него
         save_game(save, level_count);
     }
 }
@@ -51,35 +56,53 @@ void start_game(char level_path[MAX_LEVEL_PATH], Save *save){
     Game game;
     memset(&game, 0, sizeof(game));
     
+    //Безопасная загрузка уровня.
     if(level_load(&game.level, level_path)){
         printf("Invalid level\n");
         getchar();
         return;
     }
-
+    //Инициализвация системы ввода
     input_init();
+    //Нахождение координат игрока
     player_position(&game);
 
+    //Цикл уровня: выход если уровень пройден или нажата клавиша выхода
     while (game.level.total_crates > crates_on_targets(&game))
     {
+        //Ввод игрока
         int key = player_input();
 
+        //Выход из уровня
         if(key == KEY_ESCAPE){
             input_shutdown();
             return;
         }
-        if(key == KEY_LAST_MOVE){
+        //Отмена хода
+        else if(key == KEY_LAST_MOVE){
             last_move(&game);
         }
-        move_player(&game, key);
+        //Обработка передвижения
+        else move_player(&game, key);
         render_level(&game);
     }
+    //Возврат настроек терминала по умолчанию
     input_shutdown();
+    //Обновление рекорда уровня
     if(game.moves < save->best_moves || save->best_moves == 0){
         save->best_moves = game.moves;
     }
     
-    printf("\033[2J\033[HCongratulations!\nMoves: %d", game.moves);
+    system("clear");
+    printf("=========================\n");
+    printf("     CONGRATULATIONS!    \n");
+    printf("=========================\n");
+    printf("You completed the level in %d moves!\n", game.moves);
+    if (game.moves <= save->best_moves) {
+        printf(ANSI_GREEN "NEW PERSONAL BEST!" ANSI_RESET "\n");
+    }
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
     getchar();
     return;
 }
